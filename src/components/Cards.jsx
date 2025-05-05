34d450656bc7fdce8c56193f8b6f5fc93bc7efe6
Mahir Siyam <mahirmubtasimsiyam@gmail.com>
2025-05-05 19:11:02 +0600

    added card sections

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import { Link } from "react-router";
 
 const Cards = ({ data }) => {
-  const {id , name , description , thumbnail} = data;
+  const { id, name, description, thumbnail } = data;
 
   return (
     <div className="card bg-base-100 shadow-lg">
-  <figure>
-    <img className="w-[300px] h-[200px]"
-      src={thumbnail}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p className="w-9/12">{description}</p>
-    <div className="card-actions justify-end">
-      <Link to={`/card_details/${id}`} className="btn btn-primary">Read More</Link>
+      <figure>
+        <img className="w-[300px] h-[200px]" src={thumbnail} alt="running" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p className="w-9/12">{description}</p>
+        <div className="card-actions justify-end">
+          <Link to={`/card_details/${id}`} className="btn btn-primary">
+            Read More
+          </Link>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
